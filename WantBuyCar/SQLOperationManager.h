5e66ea690bd68ca60d31196915c39208194b2747
WangLuofan <wangluofan@gmail.com>
2015-05-26 14:47:29 +0800

    增加注册接口

---
+++ WantBuyCar/SQLOperationManager.h
@@ -0,0 +1,13 @@
+//
+//  SQLOperationManager.h
+//  WantBuyCar
+//
+//  Created by 王落凡 on 15/5/26.
+//  Copyright (c) 2015年 王落凡. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SQLOperationManager : NSObject
+
+@end
---
+++ WantBuyCar/SQLOperationManager.m
@@ -0,0 +1,13 @@
+//
+//  SQLOperationManager.m
+//  WantBuyCar
+//
+//  Created by 王落凡 on 15/5/26.
+//  Copyright (c) 2015年 王落凡. All rights reserved.
+//
+
+#import "SQLOperationManager.h"
+
+@implementation SQLOperationManager
+
+@end
